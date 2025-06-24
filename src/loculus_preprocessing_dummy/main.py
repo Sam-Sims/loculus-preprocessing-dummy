1516a6c246d2e00a449e62da5a10d024ef430ccc
Sam Sims <samsimss98@gmail.com>
2025-06-24 12:22:14 +0100

    debug logging

--- src/loculus_preprocessing_dummy/main.py
+++ src/loculus_preprocessing_dummy/main.py
@@ -153,14 +153,13 @@ def process(unprocessed: list[Sequence]) -> list[Sequence]:
     for sequence in unprocessed:
         metadata = sequence.data.get("metadata", {})
         for unaligned_sequence in sequence.data.get("unalignedNucleotideSequences", {}):
-            logging.info(
-                f"Processing sequence {sequence.accession} version {sequence.version} with unaligned sequence: {unaligned_sequence}"
-            )
+            print(unaligned_sequence)
+            print(type(unaligned_sequence))
             main = sequence.data.get("main", "main")
             logging.info(f"Processing main sequence: {main}")
-            if unaligned_sequence == "AAAA":
+            if main == "AAAA":
                 metadata["clade"] = "cladei"
-            elif unaligned_sequence == "AAAT":
+            elif main == "AAAT":
                 metadata["clade"] = "cladeii"
 
         processedFiles = {}
